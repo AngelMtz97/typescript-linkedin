5ff8701ff80df59a0bbf1eb902102fbbce5e089b
Angel Sangerman <armando.amartz97@gmail.com>
2024-04-19 19:17:03 -0600

    Ejemplos de clases en typescript

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,26 @@
-const mensaje = "Hola desde typescript";
-console.log(mensaje);
+type Color = 'negro' | 'rojo' | 'azul' | 'amarillo';
+
+class Vehiculo {
+    _marca: string;
+    _color: Color;
+
+    constructor(marca: string, color: Color) {
+        this._marca = marca;
+        this._color = color;
+    }
+
+    descripcionVehiculo(): string {
+        return `El vehículo es de marca ${this._marca} y es de color ${this._color}`;
+    }
+
+    moverse(): string {
+        return `El vehículo se mueve a una velocida de ${this.obtenerVelocidad()}`;
+    }
+
+    private obtenerVelocidad(): string {
+        return '100 km/h';
+    }
+}
+
+const miVehiculo = new Vehiculo('Nissan', 'negro');
+console.log(miVehiculo.descripcionVehiculo());
